f51907ae963d949040e9c022d454590d5352a827
Rafdy Fauzan Ilham Firdaus <159490815+araaapp@users.noreply.github.com>
2024-08-01 08:49:28 +0700

    Rekap Nilai Data Mahasiswa

---
+++ models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    nim = Column(String)  # Ensure this column is defined
+    presensi = Column(Integer)
+    nilai_tugas = Column(Integer)
+    nilai_uts = Column(Integer)
+    nilai_uas = Column(Integer)
+    total_nilai = Column(Integer)
+    rata_rata = Column(Integer)
+    nilai_huruf = Column(String)
+    status = Column(String)
